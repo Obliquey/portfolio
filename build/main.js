bd3a3b62e4c55c092b187edfcd239c1e2675590a
Anders Boyum <Acboyum@gmail.com>
2023-07-13 10:37:40 -0500

    playing around with js/html/css relations

---
+++ build/main.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    const square = document.getElementById("square");
+    square.classList.toggle("fullHeight");
+}
+function shiftBox() {
+    const square = document.getElementById("square");
+    square.classList.toggle("alignOther");
+}
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+function changeColor() {
+    const square = document.getElementById("square");
+    square.classList.toggle("fullHeight");
+}
+
+function shiftBox() {
+    const square = document.getElementById("square");
+    square.classList.toggle("alignOther");
+}
