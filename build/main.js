5fa167b24f08ad9226aa0313b44c2360b4ae51b7
Anders Boyum <Acboyum@gmail.com>
2023-07-14 11:08:27 -0500

    Dunno what I'm doing, feel like I'm waffling

--- build/main.js
+++ build/main.js
@@ -1,15 +1,15 @@
 let sidebarOpen = false;
 function openSideBar() {
     if (!sidebarOpen) {
-        console.log('Helloooooo');
         document.getElementById('navBar').classList.toggle('open');
+        document.getElementById('main').classList.toggle('open');
         sidebarOpen = true;
     }
 }
 function closeSideBar() {
     if (sidebarOpen) {
-        console.log('Helloooooo');
         document.getElementById('navBar').classList.toggle('open');
+        document.getElementById('main').classList.toggle('open');
         sidebarOpen = false;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ let sidebarOpen : boolean = false;
 
 function openSideBar() {
     if(!sidebarOpen){
-        console.log('Helloooooo')
         document.getElementById('navBar').classList.toggle('open');
+        document.getElementById('main').classList.toggle('open');
         sidebarOpen = true;
     }
 }
 
 function closeSideBar() {
     if(sidebarOpen){
-        console.log('Helloooooo')
         document.getElementById('navBar').classList.toggle('open');
+        document.getElementById('main').classList.toggle('open');
         sidebarOpen = false;
     }
 }
